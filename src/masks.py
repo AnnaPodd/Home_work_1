3053fa23971cffe9ad16f57f4aab695c16df1a7b
Anna Poddubko <anna_victorovna@inbox.ru>
2025-02-13 14:20:19 +0300

    Начало второй части ДЗ

---
+++ src/masks.py
@@ -0,0 +1,19 @@
+def get_mask_card_number(card_number):
+    str_card_number = str(card_number)
+    return f"{str_card_number[:4]} {str_card_number[4:6]}** **** {str_card_number[-4:]}"
+
+
+def get_mask_account(account_number):
+    str_account_number = str(account_number)
+    return f"**{str_account_number[-4:]}"
+
+
+print(get_mask_card_number(1234567890123456))
+print(get_mask_account(73654108430135874305))
+
+
+
+
+
+
+
