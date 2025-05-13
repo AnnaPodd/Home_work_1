05c3730b9a2c281a8c0a864e45cfe77945ca662d
Anna Poddubko <anna_victorovna@inbox.ru>
2025-05-13 14:15:50 +0300

    Исправлена функция подсчета по категории

--- src/filter_transactions.py
+++ src/filter_transactions.py
@@ -8,7 +8,10 @@ def search_transactions(transactions, search_string):
     return [transaction for transaction in transactions if pattern.search(transaction.get('description', ''))]
 
 
-def count_operations_by_category(transactions):
-    """Подсчитывает количество операций по категориям."""
-    descriptions = [transaction.get('description', '') for transaction in transactions]
+def count_operations_by_category(transactions, categories):
+    """Подсчитывает количество операций по указанным категориям."""
+    filtered_transactions = [transaction for transaction in transactions if transaction.get('category') in categories]
+
+    descriptions = [transaction.get('description', '') for transaction in filtered_transactions]
+
     return dict(Counter(descriptions))
